408faf011c28084ea6f89158fb59470045827526
Stormer <Samuelayodeji.ask@gmail.com>
2025-07-08 19:51:34 +0700

    hello to arithmetic operations

---
+++ basics/arith_operations.go
@@ -0,0 +1,15 @@
+package basics
+
+import "fmt"
+
+func main() {
+ 
+	var a, b int = 10, 5 
+
+	var result int
+
+	result = a + b
+	fmt.Println("Addition:", result)
+
+	// signed integeters are +ve and -ve, whilst unsigned are only positive; it can go lowest to zero, but not lower than that
+}
